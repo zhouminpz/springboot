5da4d4c40073528771d7843d07f8a1afa4ae59f2
zhoumin <zhouminpz@126.com>
2018-04-19 15:42:12 +0800

    update

--- src/main/java/com/zm/Application.java
+++ src/main/java/com/zm/Application.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Description:
  * @Author: zhoumin
  * @Date: created in 2017-12-19 14:32
  **/
+
+@EnableEurekaServer
 @SpringBootApplication
 @MapperScan("com.zm.mapper")
 public class Application {
